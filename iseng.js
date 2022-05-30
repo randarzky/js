aa6e10cb4170d1cdde9fc195e122ff60f1641987
randarzky <96353426+randarzky@users.noreply.github.com>
2022-05-30 18:33:59 +0800

    Create iseng.js

---
+++ iseng.js
@@ -0,0 +1,13 @@
+myAudio = new Audio('lagu.mp3');
+if (typeof myAudio.loop == 'boolean')
+{
+    myAudio.loop = true;
+}
+else
+{
+    myAudio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+}
+myAudio.play();
